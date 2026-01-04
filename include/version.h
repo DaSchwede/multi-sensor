a06d4473668dfce3060f420731597f02774ce367
DaSchwede <Tobias_Lehnert@web.de>
2026-01-04 22:06:33 +0100

    Logo angepasst

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
 #pragma once
 #define FW_NAME    "Multi-Sensor"
-#define FW_VERSION "0.1.0"
-#define FW_DATE    "2025-12-22"
+#define FW_VERSION "0.2.0"
+#define FW_DATE    "2026-01-04"
 #define FW_AUTHOR  "Da Schwede"
 #define FW_COPYRIGHT "Copyright (c) 2025 Da Schwede"
